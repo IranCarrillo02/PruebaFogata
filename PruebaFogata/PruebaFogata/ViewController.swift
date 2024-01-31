091a73c1b532717ef0769fbd269f655b9ff1d223
Iran Carrillo Guzmán <iran.carrillo.guzman.contractor@bbva.com>
2024-01-31 17:00:15 -0600

    First commit, and creation of project

---
+++ PruebaFogata/PruebaFogata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaFogata
+//
+//  Created by Iran Carrillo Guzman on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
